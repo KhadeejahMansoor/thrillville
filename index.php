487cdf17d6ac525be877499641096a776d6a6a48
KhadeejahMansoor <126965593+KhadeejahMansoor@users.noreply.github.com>
2024-11-30 11:24:59 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,32 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="Viewport" content="width= device-width , initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>THRILLVILLE</title>
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/header.css">
         <link rel="stylesheet" href="css/footer.css">
-         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-         <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,700;1,200;1,300;1,400&display=swap" rel="stylesheet">
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.
-4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqR
-Kc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-<link rel="stylesheet"
-href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,700;1,200;1,300;1,400&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
     </head>
     <body>
+        <!-- Banner Section -->
         <div class="banner">
+            <!-- Include Header -->
             <?php include('header.php'); ?>
-        
-
+            
+            <!-- Main Content -->
             <div class="cont">
                 <h1>WORLD'S NO 1 <br><span>FUN GALA</span></h1>
                 <a href="booking3.php"><button class="btn-2"><span>BOOK ME</span></button></a>
             </div>
         </div>
-       
+
+        <!-- Include Footer -->
         <?php include('footer.php'); ?>
     </body>
 </html>
